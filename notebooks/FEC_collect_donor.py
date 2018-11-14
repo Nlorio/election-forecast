ee7f00d537b795b3bfffe16c8424a59ceeb8f848
Nlorio <nicholas@lorio.me>
2018-11-13 23:52:49 -0800

    Fixed script, RUN THIS

--- notebooks/FEC_collect_donor.py
+++ notebooks/FEC_collect_donor.py
@@ -65,46 +65,48 @@ df_races.head()
 
 df_race_candidates = df_races[df_races.winner != '<BLANK>']
 
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+# from fuzzywuzzy import fuzz
+# from fuzzywuzzy import process
 
-cols = np.append(df_cand.columns.values, np.array(['race_candidates', 'date_of_poll', 'election_result']))
-dimension_length = 2*len(df_race_candidates.race_type) + 20 # To account for potential extra rows 
-df_selected_candidates = pd.DataFrame(np.full((dimension_length, len(cols)), np.nan), columns=cols)
-df_selected_candidates['election_years'] = df_selected_candidates['election_years'].astype(list)
+# cols = np.append(df_cand.columns.values, np.array(['race_candidates', 'date_of_poll', 'election_result']))
+# dimension_length = 2*len(df_race_candidates.race_type) + 20 # To account for potential extra rows 
+# df_selected_candidates = pd.DataFrame(np.full((dimension_length, len(cols)), np.nan), columns=cols)
+# df_selected_candidates['election_years'] = df_selected_candidates['election_years'].astype(list)
 
-j = 0
-for index, race_row in df_race_candidates.iterrows():
+# j = 0
+# for index, race_row in df_race_candidates.iterrows():
     
-    race_type = race_row.race_type
-    race_date = race_row.date
-    race_candidates = race_row.top_candidates
-    race_state = race_row.state
+#     race_type = race_row.race_type
+#     race_date = race_row.date
+#     race_candidates = race_row.top_candidates
+#     race_state = race_row.state
     
-    for index, row in df_cand.iterrows():
+#     for index, row in df_cand.iterrows():
         
-        last_name = row[0].split()[0]
-        year = row.cycles
-        race = row.office_full.lower()
-        state = row.state
+#         last_name = row[0].split()[0]
+#         year = row.cycles
+#         race = row.office_full.lower()
+#         state = row.state
 
-        for race_candidate in race_candidates:
-            score = fuzz.token_sort_ratio(last_name, race_candidate)
-            if (score >= 100) & (race == race_type) & (year == race_date.year) & (state == race_state):
-                temp_row = df_cand.iloc[index, :]
-                temp_row['race_candidates'] = race_candidates
-                temp_row['date_of_poll'] = race_row.date
-                temp_row['election_result'] = race_row.winner
-                df_selected_candidates.iloc[j,:] = temp_row
-                print(j)
-                j += 1
+#         for race_candidate in race_candidates:
+#             score = fuzz.token_sort_ratio(last_name, race_candidate)
+#             if (score >= 100) & (race == race_type) & (year == race_date.year) & (state == race_state):
+#                 temp_row = df_cand.iloc[index, :]
+#                 temp_row['race_candidates'] = race_candidates
+#                 temp_row['date_of_poll'] = race_row.date
+#                 temp_row['election_result'] = race_row.winner
+#                 df_selected_candidates.iloc[j,:] = temp_row
+#                 print(j)
+#                 j += 1
             
-df_selected_candidates 
+# df_selected_candidates 
         
             
-df_selected_candidates = df_selected_candidates.dropna()
+# df_selected_candidates = df_selected_candidates.dropna()
 
-df_selected_candidates.to_pickle('df_selected_cand.pkl')
+# df_selected_candidates.to_pickle('df_selected_cand.pkl')
+
+df_selected_candidates = pd.read_pickle('df_selected_cand.pkl')
         
 df_committee_id = pd.read_pickle('committee_ids.pkl')
 
@@ -256,88 +258,86 @@ def candidate_donor_df(candidate_name, states, df_candidate_id):
     cols = np.append(cols, donation_levels)
 
     df = pd.DataFrame(np.full((1000, len(cols)), np.nan), columns=cols)
-    
-    for cand_id in cand_ids:
-        #print(cand_id)
-    
-        cycles = int(cand_row.cycles.values[0])
+    cand_id = cand_ids[0]
+#     for cand_id in cand_ids:
+    #print(cand_id)
+
+    cycles = int(cand_row.cycles.values[0])
 
 
-        
 
-        #Iterate through candidates cycles, add all cycles as new rows. Append to each column each
-        
-        #print(cycles)
+
+    #Iterate through candidates cycles, add all cycles as new rows. Append to each column each
+
+    #print(cycles)
 #         for cycle in cycles:
-        df_size = cand_donor_size(cand_id, cycles)
-        df_state = cand_donor_state(cand_id, cycles)
-        
-        if len(df_size) > 0 & len(df_state) > 0:
-            if df_size.iloc[0,0] == 'time_out' or df_state.iloc[0,0] == 'time_out':
-                print('API Time Out, Sleep for 1 hour')
-                time.sleep(3610)
-        
-        if df_size.values.size == 0 or df_state.values.size == 0:
-            continue
+    df_size = cand_donor_size(cand_id, cycles)
+    df_state = cand_donor_state(cand_id, cycles)
 
-        donor_states = df_state.state_full
+    if len(df_size) > 0 or len(df_state) > 0:
+        if df_size.iloc[0,0] == 'time_out' or df_state.iloc[0,0] == 'time_out':
+            print('API Time Out, Sleep for 1 hour')
+            time.sleep(3610)
 
-        #Build row of information (donor_states)
+    if df_size.values.size == 0 or df_state.values.size == 0:
+        return df.dropna()
+    donor_states = df_state.state_full
 
-        # Idea
-        # Create dictionary with states as keys and initialized with 0s as values. 
-        initialize_array = np.zeros(len(states))
-        state_dict = dict(zip(states, initialize_array))
+    #Build row of information (donor_states)
 
-        for ds in donor_states:            
-            # Overwrite select values with information from df_state
-            state_dict[ds] = df_state[df_state.state_full == ds].total.values[0]
+    # Idea
+    # Create dictionary with states as keys and initialized with 0s as values. 
+    initialize_array = np.zeros(len(states))
+    state_dict = dict(zip(states, initialize_array))
 
-
-        # Add size features to state_dict 
+    for ds in donor_states:            
+        # Overwrite select values with information from df_state
+        state_dict[ds] = df_state[df_state.state_full == ds].total.values[0]
 
 
-        level_array = np.zeros(5)
-        level_dict = dict(zip(donation_levels, level_array))
-
-        for level in df_size['size']:
-            if level == 0:
-                donation_level = 'Donation Level 1'
-            if level == 200:
-                donation_level = 'Donation Level 2'
-            if level == 500:
-                donation_level = 'Donation Level 3'
-            if level == 1000:
-                donation_level = 'Donation Level 4'
-            if level == 2000:
-                donation_level = 'Donation Level 5'
-            level_dict[donation_level] = df_size[df_size['size'] == level].total.values[0]
+    # Add size features to state_dict 
 
 
-        cand_info_dict = {'name': candidate_name, 
-                          'cand_id': cand_id, 
-                          'state': cand_row.state.values[0],
-                          'cycle': cycles,
-                          'incumbent': cand_row.incumbent_challenge_full.values[0],
-                          'office_full': cand_row.office_full.values[0],
-                          'party': cand_row.party_full.values[0],
-                          'committee_id': 0, 
-                          'committee_name': 0,
-                          'race_candidates': cand_row.race_candidates.values[0],
-                          'election_result': cand_row.election_result.values[0]}
-        state_dict = merge_two_dicts(cand_info_dict, state_dict)
-        state_dict = merge_two_dicts(state_dict, level_dict)
-        #return state_dict
-        #df.iloc[j]
-        #print(np.array(list(state_dict.values())))
-        temp = list(state_dict.values())
-        df.iloc[j] = np.array(temp, dtype=object) # An array of the row information
-        j += 1
+    level_array = np.zeros(5)
+    level_dict = dict(zip(donation_levels, level_array))
+
+    for level in df_size['size']:
+        if level == 0:
+            donation_level = 'Donation Level 1'
+        if level == 200:
+            donation_level = 'Donation Level 2'
+        if level == 500:
+            donation_level = 'Donation Level 3'
+        if level == 1000:
+            donation_level = 'Donation Level 4'
+        if level == 2000:
+            donation_level = 'Donation Level 5'
+        level_dict[donation_level] = df_size[df_size['size'] == level].total.values[0]
+
+
+    cand_info_dict = {'name': candidate_name, 
+                      'cand_id': cand_id, 
+                      'state': cand_row.state.values[0],
+                      'cycle': cycles,
+                      'incumbent': cand_row.incumbent_challenge_full.values[0],
+                      'office_full': cand_row.office_full.values[0],
+                      'party': cand_row.party_full.values[0],
+                      'committee_id': 0, 
+                      'committee_name': 0,
+                      'race_candidates': cand_row.race_candidates.values[0],
+                      'election_result': cand_row.election_result.values[0]}
+    state_dict = merge_two_dicts(cand_info_dict, state_dict)
+    state_dict = merge_two_dicts(state_dict, level_dict)
+    #return state_dict
+    #df.iloc[j]
+    #print(np.array(list(state_dict.values())))
+    temp = list(state_dict.values())
+    df.iloc[j] = np.array(temp, dtype=object) # An array of the row information
+    j += 1
     return df.dropna()
 
                          
-                         
-#Add df_candidate_id input to the previous function for candidate donor information collection function 
+ #Add df_candidate_id input to the previous function for candidate donor information collection function 
 
 def get_committee_candidates(candidate_name, states, df_candidate_id, df_comm):
     cand_row = df_candidate_id[df_candidate_id.name == candidate_name]
@@ -357,96 +357,96 @@ def get_committee_candidates(candidate_name, states, df_candidate_id, df_comm):
     cols = np.append(cols, donation_levels)
 
     df = pd.DataFrame(np.full((1000, len(cols)), np.nan), columns=cols)
-    #print(cand_ids)
-    for cand_id in cand_ids:
-        comm_rows = df_comm[df_comm.candidate_ids == cand_id]
-        #return comm_rows
-        
-        #for row in comm_rows: 
-        for index, row in comm_rows.iterrows(): #Iterate through each row of the dataframe comm_row
-            #print('iter')
-            candidate_donor_weight = row.candidate_donor_weight
-            cycles = row.cycles
-            comm_id = row.committee_id
-            #Iterate through candidates cycles, add all cycles as new rows. Append to each column each
-            #print(cycles)
-            for cycle in cycles:
-                df_size = comm_donor_size(comm_id)
-                df_state = comm_donor_state(comm_id)
-                
-                if len(df_size) > 0 & len(df_state) > 0:
-                    if df_size.iloc[0,0] == 'time_out' or df_state.iloc[0,0] == 'time_out':
-                        print('API Time Out, Sleep for 1 hour')
-                        time.sleep(3610)
-                
-                if df_size.values.size == 0 or df_state.values.size == 0:
-                    continue
+    cand_id = cand_ids[0]
+#     for cand_id in cand_ids:
+    comm_rows = df_comm[df_comm.candidate_ids == cand_id]
+    #return comm_rows
 
-                donor_states = df_state.state_full
+    #for row in comm_rows: 
+    for index, row in comm_rows.iterrows(): #Iterate through each row of the dataframe comm_row
+        #print('iter')
+        candidate_donor_weight = row.candidate_donor_weight
+        cycles = row.cycles
+        comm_id = row.committee_id
+        #Iterate through candidates cycles, add all cycles as new rows. Append to each column each
+        #print(cycles)
+#         cycle = cycles[0]
+        for cycle in cycles:
+            df_size = comm_donor_size(comm_id)
+            df_state = comm_donor_state(comm_id)
 
-                #Build row of information (donor_states)
+            if len(df_size) > 0 or len(df_state) > 0:
+                if df_size.iloc[0,0] == 'time_out' or df_state.iloc[0,0] == 'time_out':
+                    print('API Time Out, Sleep for 1 hour')
+                    time.sleep(3610)
 
-                # Idea
-                # Create dictionary with states as keys and initialized with 0s as values. 
-                initialize_array = np.zeros(len(states))
-                state_dict = dict(zip(states, initialize_array))
+            if df_size.values.size == 0 or df_state.values.size == 0:
+                break
+            donor_states = df_state.state_full
 
-                for ds in donor_states:            
-                    # Overwrite select values with information from df_state
-                    state_row = df_state[df_state.state_full == ds]
-                    state_dict[ds] = state_row.total.values[0] * candidate_donor_weight
+            #Build row of information (donor_states)
 
+            # Idea
+            # Create dictionary with states as keys and initialized with 0s as values. 
+            initialize_array = np.zeros(len(states))
+            state_dict = dict(zip(states, initialize_array))
 
-                # Add size features to state_dict 
+            for ds in donor_states:            
+                # Overwrite select values with information from df_state
+                state_row = df_state[df_state.state_full == ds]
+                state_dict[ds] = state_row.total.values[0] * candidate_donor_weight
 
 
-                level_array = np.zeros(5)
-                level_dict = dict(zip(donation_levels, level_array))
-
-                for level in df_size['size']:
-                    if level == 0:
-                        donation_level = 'Donation Level 1'
-                    if level == 200:
-                        donation_level = 'Donation Level 2'
-                    if level == 500:
-                        donation_level = 'Donation Level 3'
-                    if level == 1000:
-                        donation_level = 'Donation Level 4'
-                    if level == 2000:
-                        donation_level = 'Donation Level 5'
-                    size_row = df_size[df_size['size'] == level]
-                    level_dict[donation_level] = size_row.total.values[0] * candidate_donor_weight
+            # Add size features to state_dict 
 
 
-                cand_info_dict = {'name': candidate_name, 
-                                  'cand_id': cand_id, 
-                                  'state': row.state,
-                                  'cycle': cycle,
-                                  'incumbent': 0,
-                                  'office_full': 0,
-                                  'party': 0,
-                                  'committee_id': row.committee_id,
-                                  'committee_name': row[11],
-                                  'race_candidates': 0,
-                                  'election_result': 0}
-                state_dict = merge_two_dicts(cand_info_dict, state_dict)
-                state_dict = merge_two_dicts(state_dict, level_dict)
+            level_array = np.zeros(5)
+            level_dict = dict(zip(donation_levels, level_array))
 
-                #return state_dict
+            for level in df_size['size']:
+                if level == 0:
+                    donation_level = 'Donation Level 1'
+                if level == 200:
+                    donation_level = 'Donation Level 2'
+                if level == 500:
+                    donation_level = 'Donation Level 3'
+                if level == 1000:
+                    donation_level = 'Donation Level 4'
+                if level == 2000:
+                    donation_level = 'Donation Level 5'
+                size_row = df_size[df_size['size'] == level]
+                level_dict[donation_level] = size_row.total.values[0] * candidate_donor_weight
 
 
-                #return state_dict.values()
-                df.iloc[j] = np.array(list(state_dict.values()), dtype = object) # An array of the row information
-                j += 1
+            cand_info_dict = {'name': candidate_name, 
+                              'cand_id': cand_id, 
+                              'state': row.state,
+                              'cycle': cycle,
+                              'incumbent': 0,
+                              'office_full': 0,
+                              'party': 0,
+                              'committee_id': row.committee_id,
+                              'committee_name': row[11],
+                              'race_candidates': 0,
+                              'election_result': 0}
+            state_dict = merge_two_dicts(cand_info_dict, state_dict)
+            state_dict = merge_two_dicts(state_dict, level_dict)
+
+            #return state_dict
+
+
+            #return state_dict.values()
+            df.iloc[j] = np.array(list(state_dict.values()), dtype = object) # An array of the row information
+            j += 1
+            break
     return df.dropna()
     
-
 #Time to Run 
-(len(df_selected_candidates.name) / 10 ) * 44 / 60 /60
-
-
+time = (len(df_selected_candidates.name) / 10 ) * 22 / 60 /60
+print('Hours to run, not counting sleep time: {}'.format(time))                        
+    
 cand_info = ['name', 'cand_id', 'state','cycle', 'incumbent', 'office_full', 
-             'party', 'committee_id', 'race_candidates', 'election_result']
+             'party', 'committee_id', 'committee_name', 'race_candidates', 'election_result']
 donation_levels = ['Donation Level 1',
        'Donation Level 2', 
        'Donation Level 3',
@@ -454,26 +454,39 @@ donation_levels = ['Donation Level 1',
        'Donation Level 5']
 columns = np.append(cand_info, states)
 columns = np.append(columns, donation_levels)
-df = pd.DataFrame(columns=columns)
+df = pd.DataFrame(np.full((100000, len(columns)), np.nan), columns=columns)
+
 i = 1
+j = 0 
 amount = len(df_selected_candidates.name) 
 print(datetime.now())
 
 for name in df_selected_candidates.name:
     df_name = candidate_donor_df(name, states, df_selected_candidates)
-    df = pd.concat([df, df_name], sort=False)
-    
+    for index, row in df_name.iterrows():
+        df.iloc[j] = row 
+        j += 1
+#     df = pd.concat([df, df_name], sort=False)
+
     df_name_comm = get_committee_candidates(name, states, df_selected_candidates, df_comm)
     if len(df_name_comm.name) != 0:
-        df = pd.concat([df, df_name_comm], sort=False)
+        for index, row in df_name_comm.iterrows():
+
+            df.iloc[j] = row
+            j+= 1
+        
+   
+    #df = pd.concat([df, df_name_comm], sort=False)
     print('{}/{}'.format(i, amount))
     i+=1
-    if i == 10:
-        print(datetime.datetime.now())
-        break
-df
+#     if i == 10:
+#         print(datetime.now())
+#         break
+df        
 
+df = df.dropna()
 
+df.to_pickle('donor_df.pkl')
 
 s3 = boto3.resource('s3')
 s3.Object('tenguins-tmp', 'donor_df.pkl').put(Body=pickle.dump(df))
